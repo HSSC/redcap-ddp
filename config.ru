5039d70271aead096c3ebf7f123216295c02449b
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-08-12 14:41:03 -0400

    Add bundler

---
+++ config.ru
@@ -0,0 +1,7 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+require './ddp'
+run App
--- ddp.rb
+++ ddp.rb
@@ -1,21 +1,23 @@
-require 'sinatra'
 require 'json'
 
-# metadata web service
-post '/metadata' do
-  @user       = params['user']
-  @project_id = params['project_id']
-  @redcap_url = params['redcap_url']
-end
+class App < Sinatra::Application
 
-# data web service
-post '/data' do
-  request.body.rewind
-  req         = JSON.parse request.body.read
+  # metadata web service
+  post '/metadata' do
+    @user       = params['user']
+    @project_id = params['project_id']
+    @redcap_url = params['redcap_url']
+  end
 
-  @user       = req['user']
-  @project_id = req['project_id']
-  @redcap_url = req['redcap_url']
-  @id         = req['id']
-  @fields     = req['fields']
+  # data web service
+  post '/data' do
+    request.body.rewind
+    req         = JSON.parse request.body.read
+
+    @user       = req['user']
+    @project_id = req['project_id']
+    @redcap_url = req['redcap_url']
+    @id         = req['id']
+    @fields     = req['fields']
+  end
 end
