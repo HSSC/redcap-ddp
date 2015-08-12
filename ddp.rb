c2b975044864d79487a8e03fd464357ca21ab708
Jerry Hardee <hardeeje@musc.edu>
2015-08-12 13:59:35 -0400

    Define routes for the metadata and data web services.

---
+++ ddp.rb
@@ -0,0 +1,21 @@
+require 'sinatra'
+require 'json'
+
+# metadata web service
+post '/metadata' do
+  @user       = params['user']
+  @project_id = params['project_id']
+  @redcap_url = params['redcap_url']
+end
+
+# data web service
+post '/data' do
+  request.body.rewind
+  req         = JSON.parse request.body.read
+
+  @user       = req['user']
+  @project_id = req['project_id']
+  @redcap_url = req['redcap_url']
+  @id         = req['id']
+  @fields     = req['fields']
+end
