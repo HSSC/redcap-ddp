69b21dcb5644506c4f36fb1f0e6335cda08f87aa
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-09-25 14:34:09 -0400

    Refactor QueryString.rb

--- query.rb
+++ query.rb
@@ -7,18 +7,11 @@ module GlobalNamespace
     def initialize(id, fields)
       @id = id
       @fields = fields
-      @temporal_fields, @non_temporal_fields = fields.partition { |field| field['timestamp_max'] && field['timestamp_min'] }
     end
     
     def execute
-      #puts QueryString.new(@id).
-      #  sql_select(@temporal_fields + [{'field' => TIMESTAMP_COLUMN}]).
-      #  to_s
-
       results      = []
-      query_string = QueryString.new(@id).
-        sql_select(@non_temporal_fields).
-        to_s
+      query_string = QueryString.new(@id, @fields).to_s
       rows         = client.execute(query_string)
 
       rows.each do |row| 
--- query_string.rb
+++ query_string.rb
@@ -1,70 +1,81 @@
 require 'time'
 
 module GlobalNamespace
+  
   class QueryString
     PATIENTS_TABLE      = 'EDW2.Patients'
     ORDER_RESULTS_TABLE = 'EDW2.Order_Results'    
     TIMESTAMP_COLUMN    = "OrderDTM"
     
-    def initialize(id)
+    def initialize(id, fields)
       @id          = id
-      @columns     = []
-      @where       = ["#{PATIENTS_TABLE}.SourceSystem=\"EPIC\"", "#{PATIENTS_TABLE}.PatientExternalID=\"#{@id}\""]
-      @from        = PATIENTS_TABLE
-      @joins       = []
-      @joins_on    = []
-      @time_ranges = []
-    end
-    
-    def sql_select(columns)
-      columns.each do |col|
-        @columns << col['field']
-        if col['timestamp_max'] && col['timestamp_min']
-          @time_ranges << {from: Time.strptime(col['timestamp_min'], '%Y-%m-%d %H:%M:%S'), to: Time.strptime(col['timestamp_max'], '%Y-%m-%d %H:%M:%S')}
-        end
-      end
-      self
-    end
-    
-    def from(table)
-      @from = table
-      self
-    end
-    
-    def join(table, on)
-      @joins    << table
-      @joins_on << on
-      self
-    end
-    
-    def where(*conjuncts)
-      @where += conjuncts
-      self
+      @fields      = fields
+      @temporal_fields, @non_temporal_fields = @fields.partition { |field| field['timestamp_max'] && field['timestamp_min'] }
     end
     
     def to_s
-      @columns.uniq!
-
-      select_clause = "SELECT #{@columns.map { |col| prefix(col) }.join(',')}"
-      from_clause   = "FROM #{@from}"
-      unless @joins.empty?
-        from_clause += " JOIN #{@joins[0]} ON #{@joins_on[0]}"
-      end
-      
-      where_clause  = "WHERE #{@where.join(' AND ')}"
-      
-      unless @time_ranges.empty?
-        # merge time ranges into a gross time range
-        gross_from = @time_ranges.map { |tr| tr[:from] }.min.strftime('%Y-%m-%d %H:%M:%S')
-        gross_to   = @time_ranges.map { |tr| tr[:to] }.max.strftime('%Y-%m-%d %H:%M:%S')
-        
-        where_clause += %Q{ AND (DATEFORMAT(#{prefix(TIMESTAMP_COLUMN)}, 'YYY-MM-DD HH:MM:SS') BETWEEN "#{gross_from}" AND "#{gross_to}")}
-      end
-      
-      "#{select_clause} #{from_clause} #{where_clause}"
+      [
+	select_clause,
+	from_clause,
+	join_clause,
+	where_clause
+      ].compact.join(' ')
     end
     
     private
+
+    def select_clause
+      "SELECT #{columns}"
+    end
+
+    def from_clause
+      "FROM #{PATIENTS_TABLE}"
+    end
+
+    def where_clause
+      clause = "WHERE #{where_default}"
+
+      if time_ranges.any?
+        gross_from = time_ranges.map { |tr| tr[:from] }.min.strftime('%Y-%m-%d %H:%M:%S')
+        gross_to   = time_ranges.map { |tr| tr[:to] }.max.strftime('%Y-%m-%d %H:%M:%S')
+
+	clause += %Q{ AND (DATEFORMAT(#{prefix(TIMESTAMP_COLUMN)}, 'YYY-MM-DD HH:MM:SS') BETWEEN "#{gross_from}" AND "#{gross_to}")}
+      end
+
+      clause
+    end
+
+    def join_clause
+      if @temporal_fields.any?
+	"JOIN #{join_table} ON #{join_on}"
+      end
+    end
+
+    def join_table
+      field = @temporal_fields.first['field']
+      
+      if field_row = GlobalNamespace.global_settings[:metadata].find { |row| row.has_value?(field) }
+	@join_table ||= field_row['category']
+      end
+    end
+
+    def join_on
+      "#{PATIENTS_TABLE}.PatientExternalID = #{@join_table}.PatientExternalID"
+    end
+
+    def columns
+      @non_temporal_fields.uniq.map { |field| prefix(field) }.join(',')
+    end
+
+    def time_ranges
+      @temporal_fields.map do |field|
+        { from: Time.strptime(field['timestamp_min'], '%Y-%m-%d %H:%M:%S'), to: Time.strptime(field['timestamp_max'], '%Y-%m-%d %H:%M:%S') }
+      end
+    end
+
+    def where_default
+      "#{PATIENTS_TABLE}.SourceSystem=\"EPIC\"", "#{PATIENTS_TABLE}.PatientExternalID=\"#{@id}\""
+    end
     
     def prefix(column)
       # casecmp returns 0 if strings are equal, ignoring case
