4cbbec5ba42206693901f3eb49d7b3cb7ed4c2b8
Greg Oleksiak <greg.oleksiak@gmail.com>
2015-09-25 16:32:54 -0400

    Attach timestamps to results containing temporal fields.

--- query.rb
+++ query.rb
@@ -2,7 +2,7 @@ module GlobalNamespace
 
   class Query < DbClient
 
-    TIMESTAMP_COLUMN    = "OrderDTM"
+    TIMESTAMP_COLUMN    = "ResultsDTM"
 
     def initialize(id, fields)
       @id = id
@@ -12,10 +12,19 @@ module GlobalNamespace
     def execute
       results      = []
       query_string = QueryString.new(@id, @fields).to_s
+      puts "DEBUG: #{query_string}"
       rows         = client.execute(query_string)
 
       rows.each do |row| 
-        row.each { |key, value| results.push({ field: key, value:  value }) }
+        timestamp = row[TIMESTAMP_COLUMN]
+        row.delete(TIMESTAMP_COLUMN)
+        row.each do |key, value|
+          if timestamp
+            results.push({ field: key, value: value, timestamp: timestamp })
+          else
+            results.push({ field: key, value: value })
+          end
+        end
       end
       
       results
